8b4b32352a8d8c8179bd58f3d71e15002236bb73
navent <18219017@std.stei.itb.ac.id>
2023-03-27 00:37:14 +0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
+/* eslint-disable react/no-unknown-property */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Raleway, Montserrat } from 'next/font/google'
+
+const raleway = Raleway({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				:root {
+					--raleway-font: ${raleway.style.fontFamily};
+					--montserrat-font: ${montserrat.style.fontFamily};
+				}
+			`}</style>
+			<Component {...pageProps} />
+		</>
+	)
 }
