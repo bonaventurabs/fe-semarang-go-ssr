deb5998e20eed6cdb3bc80e19e7d641ded170443
navent <18219017@std.stei.itb.ac.id>
2023-05-14 19:42:24 +0700

    fix: pwa manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,13 +21,23 @@ export default function Document() {
 				<meta name="msapplication-tap-highlight" content="no" />
 				<meta name="theme-color" content="#930d20" />
 
-				<link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-				<link rel="apple-touch-icon" sizes="152x152" href="/icon.png" />
+				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+				{/* <link rel="apple-touch-icon" sizes="152x152" href="/icon.png" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
-				<link rel="apple-touch-icon" sizes="167x167" href="/icon.png" />
+				<link rel="apple-touch-icon" sizes="167x167" href="/icon.png" /> */}
 
-				<link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
 				<link rel="manifest" href="/manifest.json" />
 				{/* <link
 					rel="mask-icon"
