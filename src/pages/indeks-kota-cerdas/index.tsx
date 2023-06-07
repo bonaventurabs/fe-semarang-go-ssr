f6dcda6aea0a3d1693451a69310cd049dea374be
navent <18219017@std.stei.itb.ac.id>
2023-06-07 17:46:35 +0700

    fix: service search card description

--- src/pages/indeks-kota-cerdas/index.tsx
+++ src/pages/indeks-kota-cerdas/index.tsx
@@ -8,21 +8,18 @@ import Header from '@/containers/header/Header'
 import styles from './index.module.scss'
 
 const SmartCityIndexPage = () => {
+	const title = 'Indeks Kota Cerdas'
 	return (
 		<>
 			<Head>
-				<title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+				<title>{title}</title>
 				<meta
 					name="description"
 					content={process.env.NEXT_PUBLIC_APP_DESCRIPTION}
 				/>
-				<meta name="keywords" content={process.env.NEXT_PUBLIC_APP_KEYWORDS} />
-				<meta name="author" content={process.env.NEXT_PUBLIC_COMPANY_NAME} />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Header
-				title="Indeks Kota Cerdas"
+				title={title}
 				isBackButtonDisplayed={false}
 				isSearchButtonDisplayed
 			/>
