78cd989cd8b6280d34ef19dad87c1e8311feef87
navent <18219017@std.stei.itb.ac.id>
2023-07-24 16:58:03 +0700

    fix: sitemap.xml

--- src/pages/api/proxy/news.ts
+++ src/pages/api/proxy/news.ts
@@ -37,7 +37,7 @@ const proxyMiddleware: any = createProxyMiddleware({
 })
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.setHeader('Cache-Control', 's-maxage=1800')
+	res.setHeader('Cache-Control', 's-maxage=600')
 	proxyMiddleware(req, res, (result: any) => {
 		if (result instanceof Error) {
 			throw result
