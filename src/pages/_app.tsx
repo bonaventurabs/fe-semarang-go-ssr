a9fa6104ef4f221a8b6dbace829d5558d083ec80
navent <18219017@std.stei.itb.ac.id>
2023-07-24 14:07:02 +0700

    fix: dynamically import recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/no-unknown-property */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 import { Raleway, Montserrat } from 'next/font/google'
 import Head from 'next/head'
-import { RecoilRoot } from 'recoil'
 
 import Layout from '@/containers/layout/Layout'
 import styles from '@/styles/Home.module.scss'
+
+const RecoilRootDynamic = dynamic(
+	async () => await import('recoil').then((mod) => mod.RecoilRoot),
+	{
+		ssr: false,
+	},
+)
+
 const raleway = Raleway({ subsets: ['latin'] })
 const montserrat = Montserrat({ subsets: ['cyrillic'] })
 
@@ -23,13 +31,13 @@ export default function App({ Component, pageProps }: AppProps) {
 					--montserrat-font: ${montserrat.style.fontFamily};
 				}
 			`}</style>
-			<RecoilRoot>
+			<RecoilRootDynamic>
 				<main className={styles.app}>
 					<Layout>
 						<Component classN {...pageProps} />
 					</Layout>
 				</main>
-			</RecoilRoot>
+			</RecoilRootDynamic>
 		</>
 	)
 }
