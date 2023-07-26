4d36ee1573930e7b7b376562513c3429a5d78dd0
navent <18219017@std.stei.itb.ac.id>
2023-07-26 17:20:52 +0700

    fix: hover in mobile

--- src/components/dateSlider/DateSlider.tsx
+++ src/components/dateSlider/DateSlider.tsx
@@ -143,12 +143,11 @@ const DateSlider = ({ value, onChange }: DateSliderProps) => {
 	const [buttonStyle, setButtonStyle] = useState({ opacity: 0 })
 	useEffect(() => {
 		if (swiperRef.current) {
-			setDisabledPrev(swiperRef.current.isBeginning)
-			setDisabledNext(swiperRef.current.isEnd)
-			swiperRef.current.update()
 			swiperRef.current.on('slideChange', () => {
 				setDisabledPrev(swiperRef.current?.isBeginning)
-				setDisabledNext(swiperRef.current?.isEnd)
+			})
+			swiperRef.current.on('reachEnd', function () {
+				setDisabledNext(true)
 			})
 		}
 	}, [swiperRef])
